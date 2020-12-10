54ed0eb6b1eb1c06b5948ce6f6ae74106cabc2cb
jplaurent01 <josepablo.laurent@gmail.com>
2020-12-10 14:33:22 -0600

    comentarios

--- firebase-authentication-android-master/app/src/main/java/com/project/foodmaps/prueba.java
+++ firebase-authentication-android-master/app/src/main/java/com/project/foodmaps/prueba.java
@@ -6,22 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class prueba extends AppCompatActivity {
 
+    private EditText micaja;
+    private TextView misalida;
+    private Button botonMostar;
+    public static final String EXTRA_MESSAGE = "com.project.foodmaps.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prueba);
         Button btn_move;
         btn_move = (Button) findViewById(R.id.btnmove);
+        botonMostar = (Button) findViewById(R.id.button3);
+        micaja = (EditText) findViewById(R.id.txt);
+        misalida = (TextView) findViewById(R.id.textView9);
+
+        botonMostar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String minombre = null;
+                minombre = micaja.getText().toString();
+                misalida.setText(minombre);
+            }
+        });
+
         btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),MoveMarker.class));
             }
         });
+
     }
+
+
 }
